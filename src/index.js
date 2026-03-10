fed7014742f96f6ebb1e628b8b9730d4eb3a60f6
lydieuwayezu <uwayezulydie1w@gmail.com>
2026-03-10 21:11:32 +0200

    functioning our index file

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// This is the entry point of our React application
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Get the root element from HTML and render our App component
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
